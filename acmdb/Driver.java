a1f50769d6b6d9209eca12479979e8dcbc910983
sy15 <sy15@sjtu.edu.cn>
2014-12-06 09:53:07 +0800

    draft

--- acmdb/Driver.java
+++ acmdb/Driver.java
@@ -109,22 +109,9 @@ public class Driver {
 		}
 	}
 	
-	public static void displayMenu() {
-		System.out.println("        Bookstore Management System     ");
-		System.out.println("1. enter your own query:");
-		System.out.println("2. enter your own update:");
-		System.out.println("3. exit:");
-		System.out.println("4. clear the tables (be careful)");
-		System.out.println("5. show the funtionality menu");
-		System.out.println("please enter your choice:");
-	}
-	
 	public static void main(String[] args) {
 		System.out.println("Welcome to our Bookstore");
 		Connector con = null;
-		String choice;
-		int c = 0;
-		String sql = null;
 		try {
 			con = new Connector();
 			System.out.println("Database connection established");
@@ -133,65 +120,10 @@ public class Driver {
 			Functionality.setConfiguration(con.stmt);
 			Functionality.init();
 			
-			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-			
 			while (true) {
-				displayMenu();
-				while ((choice = in.readLine()) == null);
-				try {
-					c = Integer.parseInt(choice);
-				} catch (Exception e) {
-					continue;
-				}
+				BookStore.login();
 				
-				if (c < 1 || c > CHOICE) continue;
-				
-				if (c == 1) {
-					System.out.println("please enter your query below:");
-					while ((sql = in.readLine()) == null)
-						System.out.println(sql);
-					ResultSet rs = con.stmt.executeQuery(sql);
-					ResultSetMetaData rsmd = rs.getMetaData();
-					int numCols = rsmd.getColumnCount();
-					while (rs.next()) {
-						for (int i = 1; i <= numCols; ++i)
-							System.out.print(rs.getString(i) + "  ");
-						System.out.println("");
-					}
-					System.out.println(" ");
-					rs.close();
-				}
-				else if (c==2){
-					System.out.println("please enter your operation below:");
-					while ((sql = in.readLine()) == null && sql.length() == 0)
-						System.out.println(sql);
-					int res =con.stmt.executeUpdate(sql);
-					if (res != -1) {
-						System.out.println("Update suscess"); 
-					}
-				}
-				else if (c == 4){
-					clearTables();
-				}
-				else if (c == 5){
-					Functionality.printFuncMenu();
-					String funcChoice;
-					while ((funcChoice = in.readLine()) == null && funcChoice.length() == 0);
-					int func;
-					try {
-						func = Integer.parseInt(funcChoice);
-					} catch (Exception e) {
-						continue;
-					}
-					if (func > FUNCTIONALITY) 
-						System.out.println("out of the range");
-					else handleFunctionality(func);
-				}
-				else {//c == 3
-					System.out.println("Welcome to the next visit :D");
-					con.stmt.close();
-					break;
-				}
+				UserInterface.run(BookStore.authority);
 			}
 		} catch (Exception e) {
 			if (e instanceof com.mysql.jdbc.exceptions.jdbc4.MySQLSyntaxErrorException) {
