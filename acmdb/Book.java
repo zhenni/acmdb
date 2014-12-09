d5fc42950e24adaca9a7a50570fcba5db7ed152d
Zhen Wei <94hazelnut@gmail.com>
2014-12-09 19:58:27 +0800

    2-degree the authors are the same

--- acmdb/Book.java
+++ acmdb/Book.java
@@ -189,6 +189,11 @@ public class Book {
 	 * AND `A' and `B' are not 1-degree away at the same time.</p>
 	 * @throws SQLException */
 	public static void giveSeparationDegree(String author1, String author2) throws Exception {
+		if (author1.equals(author2)){
+			System.out.println("The 2 names of author are the same.");
+			return;
+		}
+		
 		String sql = "CREATE OR REPLACE VIEW degree AS"
 				+ "SELECT W1.author_id AS a1, W2.author_id AS a2, W1.isbn AS isbn "
 				+ "FROM writes W1, writes W2 "
